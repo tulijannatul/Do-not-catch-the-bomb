d1fefc3d9d5c0fa93f5d0fefb79648adfd151692
JannatulTuli <tuli120674@gmail.com>
2019-07-16 02:03:23 +0600

    first commit

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreController : MonoBehaviour {
+
+	//public GameObject hat;
+
+	public Text scoreText;
+
+	public int score;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		scoreText.text = score.ToString ();
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D target)
+	{
+		if (target.tag == "Bomb") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		
+
+
+		}	
+	}
+
+	void OnTriggerExit2D(Collider2D target)
+	{
+		if (target.tag == "Fruit") {
+			Destroy (target.gameObject);
+			score++;
+		}
+	}
+
+	//IEnumerator WaitTilRestart()
+	//{
+		//yield return new WaitForSeconds (1.5f);
+		//SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	//}
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Don-t-catch-the-bomb
